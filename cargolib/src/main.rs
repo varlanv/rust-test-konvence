60bf63480c8258f077d3bf074c634dbb81627f64
Vladyslav Varlan <varlanv96@gmail.com>
2025-06-27 02:15:49 +0300

    Add parent cargo

--- cargolib/src/main.rs
+++ cargolib/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    /// should qwe
+    #[test]
+    fn one_result_case_insensitive() {
+        let a = "\
+        "
+        .to_string();
+
+        assert_eq!(a, "");
+    }
+}
